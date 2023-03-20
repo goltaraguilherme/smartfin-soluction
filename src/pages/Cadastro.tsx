7fe51e2f907aa57e3fcccc25d5f77ae24e5499f9
Igor <igor_cesari@hotmail.com>
2023-03-20 16:17:21 -0300

    all

--- src/pages/Cadastro.tsx
+++ src/pages/Cadastro.tsx
@@ -1,5 +1,5 @@
 import { Header } from "../components/Header";
-import styles from '../../login.module.css'
+import styles from '../login.module.css'
 
 export default function Cadastro() {
   return (
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,7 @@
 import { FormEvent, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Header } from "../components/Header";
-import styles from '../../login.module.css'
+import styles from '../login.module.css'
 
 export default function Login() {
 
